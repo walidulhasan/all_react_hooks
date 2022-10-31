ac97e2187dae4ded7250247e5748748776801871
walidulhasan <walidulhasanboniamin@gmail.com>
2022-10-31 22:04:25 +0600

    useState Primary way

--- src/hooks/useState/UseState.jsx
+++ src/hooks/useState/UseState.jsx
@@ -1,9 +1,39 @@
-import React,{useState} from 'react'
-
+import React, { useState } from 'react';
+import ControlPointOutlinedIcon from '@mui/icons-material/ControlPointOutlined';
+import IndeterminateCheckBoxOutlinedIcon from '@mui/icons-material/IndeterminateCheckBoxOutlined';
 function UseStateHook() {
-  const [set,setGet]=useState(0);
+  const [count, setCount] = useState(0);
+
+  const countCounter = () => {
+    if (count>=100) {
+      alert("Already Crouse 100 you can't increment morethen")
+    }else{
+      setCount(()=>count+10)
+    }
+  };
+
   return (
-    <div>This is Use State Hooks {set}</div>
-  )
+    <>
+      <div>
+        <button
+          onClick={() => {
+            countCounter();
+          }}
+        >
+          <span>
+            <ControlPointOutlinedIcon />
+          </span>
+        </button>
+        <div>This is Use State Hooks {count}</div>
+        <button
+          onClick={() => (count===0?alert("You can't get Negative Value") :setCount(count-1))}
+        >
+          <span>
+            <IndeterminateCheckBoxOutlinedIcon />
+          </span>
+        </button>
+      </div>
+    </>
+  );
 }
-export default UseStateHook
+export default UseStateHook;
